c367f63b1b3ccf0a06f2792f06692207162dc7f6
ArdaWeasley <ardapaydnn@gmail.com>
2025-11-23 17:14:05 +0300

    rename logged out tokens table

--- packages/server/src/database/schemas/loggedOut.ts
+++ packages/server/src/database/schemas/loggedOut.ts
@@ -1,6 +1,6 @@
 import { mysqlTable, timestamp, varchar } from "drizzle-orm/mysql-core";
 
-export const loggedOutTokensTable = mysqlTable("loggedOutTokens", {
+export const loggedOutTokensTable = mysqlTable("logged_out_tokens", {
   token: varchar("token", { length: 255 }).notNull().primaryKey(),
   loggedOutAt: timestamp("logged_out_at").defaultNow(),
 });
